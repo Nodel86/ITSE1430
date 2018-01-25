a45e602d7c0b85be7fce61f762d167bb26d65892
ashton.harris <ashton.harris@my.tccd.edu>
2018-01-24 21:41:50 -0600

    Change1-24-18

---
+++ Classwork/Section1/Nile.Host/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+
+
+
+namespace Nile.Host
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+        }
+
+
+        static void PlayingWithPrimitives()
+        {
+            //Primitive
+            decimal unitPrice = 10.5M;
+
+            //Real decalaration
+            System.Decimal unitPrice2 = 10.5M;
+
+            //Current
+            System.DateTime now = System.DateTime.Now;
+
+            System.Collections.ArrayList items;
+
+        }
+           static void PlayingWithVariables()..
+        
+
+      
+         //Single decls
+                int hours = 0;
+                //Don't
+                //int hours;
+                //hours =0;
+                double rate = 10.25;
+
+                if (false)
+                    hours = 0;
+
+                int hours2 = hours;
+
+                //Multiple decls
+                string firstName, lastName;
+
+                //string @class;
+
+                //Single assignment
+                firstName = "Bob";
+                lastName = "Miller";
+
+                // Multiple assignment
+                firstName = lastName = "Sue";
+
+                //Math ops
+                int x = 0, y = 10;
+                int add = x + y;
+                int subtract = x - y;
+                int multiply = x * y;
+                int divide = x / y;
+                int modulous = x % y;
+
+                //x = x + 10;
+                x += 10;
+                double ceiling = Math.Ceiling(rate);
+                double flooor = ceiling;
+            }
+
+
+
+}
