79bddf1163054af05d1d8e66db5022176368012b
ashton.harris <ashton.harris@my.tccd.edu>
2018-01-29 21:38:44 -0600

    changes

--- Classwork/Section1/Nile.Host/Program.cs
+++ Classwork/Section1/Nile.Host/Program.cs
@@ -9,6 +9,128 @@ namespace Nile.Host
     {
         static void Main( string[] args )
         {
+            bool quit = false;
+            while (!quit)
+            {
+                //Display menu
+                char choice = DisplayMenu();
+
+                //Process menu selection
+                switch (choice)
+                {
+                    // case "l":
+                    case 'L':
+                    ListProducts();
+                    break;
+
+                    case 'a':
+                    case 'A':
+                    AddProduct();
+                    break;
+
+                    case 'q':
+                    case 'Q':
+                    quit = true;
+                    break;
+                };
+            };
+        }
+
+        static void AddProduct()
+        {
+            //Get name
+            _name = ReadString("Enter name:", true);
+
+            //Get Price
+            _price = ReadDecimal("Enter price:", 0);
+
+            //Get description
+            _description = ReadString("Enter optional description:", false);
+
+        }
+        private static decimal ReadDecimal( string message, decimal minValue )
+
+        {
+            do
+            {
+                Console.Write(message);
+
+                string value = Console.ReadLine();
+
+                decimal result;
+                //  if (Decimal.TryParse(value, out decimal result))
+                {
+
+                    //If not required or not empty
+                    //    if (result >= minValue)
+                  //  return result;
+                };
+
+                Console.WriteLine("Value must be >={0}", minValue);
+
+            } while (true);
+        }
+        private static string ReadString( string message, bool isRequired )
+        {
+            do
+            {
+                Console.Write(message);
+
+                string value = Console.ReadLine();
+
+
+
+                //If not required or not empty
+                //if (result >= minValue)
+                // return result;
+                return value;
+
+
+                // Console.WriteLine("Value must be >={0}", minValue);
+
+            } while (true);
+        }
+
+        static void ListProducts()
+        {
+            //Are there any products?
+            if (_name != null && _name != "")
+            {
+
+                //Display a product
+                Console.WriteLine(_name);
+                Console.WriteLine(_price);
+                Console.WriteLine(_description);
+            } else
+                Console.WriteLine("No products");
+        }
+
+        //Data for a product
+        static string _name;
+        static decimal _price;
+        static string _description;
+
+
+        private static char DisplayMenu()
+        {
+            do
+            {
+                Console.WriteLine("L)ist Products");
+                Console.WriteLine("A)dd Products");
+                Console.WriteLine("Q)uit");
+
+                string input = Console.ReadLine();
+
+                if (input == "L" || input == "l")
+                    return input[0];
+                else if (input == "A")
+                    return input[0];
+                else if (input == "Q")
+                    return input[0];
+
+                Console.WriteLine("Please choose a valid option");
+            } while (true);
+
         }
 
 
@@ -26,48 +148,50 @@ namespace Nile.Host
             System.Collections.ArrayList items;
 
         }
-           static void PlayingWithVariables()..
-        
+        static void PlayingWithVariables()
 
-      
-         //Single decls
-                int hours = 0;
-                //Don't
-                //int hours;
-                //hours =0;
-                double rate = 10.25;
+        {
 
-                if (false)
-                    hours = 0;
+            //Single decls
+            int hours = 0;
+            //Don't
+            //int hours;
+            //hours =0;
+            double rate = 10.25;
 
-                int hours2 = hours;
+            if (false)
+                hours = 0;
 
-                //Multiple decls
-                string firstName, lastName;
+            int hours2 = hours;
 
-                //string @class;
+            //Multiple decls
+            string firstName, lastName;
 
-                //Single assignment
-                firstName = "Bob";
-                lastName = "Miller";
+            //string @class;
 
-                // Multiple assignment
-                firstName = lastName = "Sue";
+            //Single assignment
+            firstName = "Bob";
+            lastName = "Miller";
 
-                //Math ops
-                int x = 0, y = 10;
-                int add = x + y;
-                int subtract = x - y;
-                int multiply = x * y;
-                int divide = x / y;
-                int modulous = x % y;
+            // Multiple assignment
+            firstName = lastName = "Sue";
 
-                //x = x + 10;
-                x += 10;
-                double ceiling = Math.Ceiling(rate);
-                double flooor = ceiling;
-            }
+            //Math ops
+            int x = 0, y = 10;
+            int add = x + y;
+            int subtract = x - y;
+            int multiply = x * y;
+            int divide = x / y;
+            int modulous = x % y;
+
+            //x = x + 10;
+            x += 10;
+            double ceiling = Math.Ceiling(rate);
+            double flooor = ceiling;
+        }
 
 
 
+    }
 }
+
