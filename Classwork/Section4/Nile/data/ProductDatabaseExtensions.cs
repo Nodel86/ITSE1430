48ef3c49680b3cc99ecf3e3e5534610bf9747fc2
Ashton Harris <ashton.harris@my.tccd.edu>
2018-03-30 13:30:28 -0500

    changes

--- Classwork/Section4/Nile/data/ProductDatabaseExtensions.cs
+++ Classwork/Section4/Nile/data/ProductDatabaseExtensions.cs
@@ -6,26 +6,48 @@ using System.Threading.Tasks;
 
 namespace Nile.Data
 {
-   public static class ProductDatabaseExtensions
+    /// <summary>Provides extension methods for <see cref="IProductDatabase"/>.</summary>
+
+    public static class ProductDatabaseExtensions
+
     {
+
         /// <summary>Seeds the database.</summary>
+
         /// <param name="source">The source.</param>
+
         public static void Seed( this IProductDatabase source )
+
         {
-            var message = "";
+
             source.Add(new Product() {
+
                 Name = "iPhone X",
+
                 IsDiscontinued = true,
-                Price = 1500, }, out message);
+
+                Price = 1500, });
+
             source.Add(new Product() {
+
                 Name = "Windows Phone",
+
                 IsDiscontinued = true,
-                Price = 15, }, out message);
+
+                Price = 15, });
+
             source.Add(new Product() {
+
                 Name = "Samsung S8",
+
                 IsDiscontinued = false,
+
                 Price = 800
-            }, out message);
+
+            });
+
         }
+
     }
+
 }
