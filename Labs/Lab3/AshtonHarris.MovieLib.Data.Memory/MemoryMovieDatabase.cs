fee77231aec33bf8a51bdcfa704dcfc6c2a5bc95
ashton.harris <ashton.harris@my.tccd.edu>
2018-04-30 17:08:40 -0500

    changes

---
+++ Labs/Lab3/AshtonHarris.MovieLib.Data.Memory/MemoryMovieDatabase.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AshtonHarris.MovieLib.Data.Memory
+{
+    /// <summary>Provides an in-memory movie database.</summary>
+    public class MemoryMovieDatabase : MovieDatabase
+    {
+
+        protected override Movie AddCore( Movie movie )
+        {
+            //Clone the object
+            movie.Id = _nextId++;
+            _movies.Add(Clone(movie));
+
+            // Return the copy
+            return movie;
+        }
+
+        protected override Movie UpdateCore( Movie movie )
+        {
+            //Clone the object
+            var existing = GetCore(movie.Id);
+            Copy(existing, movie);
+
+            //Return a copy
+            return movie;
+        }
+
+        protected override IEnumerable<Movie> GetAllCore()
+        {
+            foreach (var movie in _movies)
+            {
+                if (movie != null)
+                    yield return Clone(movie);
+            };
+        }
+
+        protected override void RemoveCore( int id )
+        {
+            var existing = GetCore(id);
+            if (existing != null)
+                _movies.Remove(existing);
+        }
+
+        private Movie Clone( Movie item )
+        {
+            var newMovie = new Movie();
+            Copy(newMovie, item);
+
+            return newMovie;
+        }
+
+        private void Copy( Movie target, Movie source )
+        {
+            target.Id = source.Id;
+            target.Title = source.Title;
+            target.Description = source.Description;
+            target.Length = source.Length;
+            target.IsOwned = source.IsOwned;
+        }
+
+        protected override Movie GetCore( int id )
+        {
+            foreach (var movie in _movies)
+            {
+                if (movie.Id == id)
+                    return movie;
+            };
+
+            return null;
+        }
+
+        protected override Movie GetMovieByTitleCore( string title )
+        {
+            foreach (var movie in _movies)
+            {
+                if (String.Compare(movie.Title, title, true) == 0)
+                    return movie;
+            };
+
+            return null;
+        }
+        private Movie GetById( int id )
+        {
+            foreach (var movie in _movies)
+            {
+                if (movie.Id == id)
+                    return movie;
+            };
+
+            return null;
+        }
+
+        private readonly List<Movie> _movies = new List<Movie>();
+        private int _nextId = 1;
+    }
+}
+
