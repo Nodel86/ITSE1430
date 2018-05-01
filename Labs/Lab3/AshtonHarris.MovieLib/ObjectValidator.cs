f3cb0ee850e5e785633a97a3348c043028009557
Ashton Harris <ashton.harris@my.tccd.edu>
2018-05-01 12:45:58 -0500

    changes

---
+++ Labs/Lab3/AshtonHarris.MovieLib/ObjectValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AshtonHarris.MovieLib
+{
+    public static class ObjectValidator
+    {
+        /// <summary>Enumerates through database to determine any errors.</summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IEnumerable<ValidationResult> Validate( this IValidatableObject source )
+        {
+            var context = new ValidationContext(source);
+            var errors = new Collection<ValidationResult>();
+            Validator.TryValidateObject(source, context, errors, true);
+
+            return errors;
+        }
+    }
+
+}
+
