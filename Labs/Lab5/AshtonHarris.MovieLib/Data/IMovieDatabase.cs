0c1f5a629aa039ccffd677ea49d0a1e2329ee37b
ashton.harris <ashton.harris@my.tccd.edu>
2018-05-01 16:34:30 -0500

    changes

---
+++ Labs/Lab5/AshtonHarris.MovieLib/Data/IMovieDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AshtonHarris.MovieLib.Data
+{
+    public interface IMovieDatabase
+    {
+        Movie Add( Movie movie, out string message );
+        Movie Update( Movie movie, out string message );
+        IEnumerable<Movie> GetAll();
+        void Remove( int id );
+    }
+}
