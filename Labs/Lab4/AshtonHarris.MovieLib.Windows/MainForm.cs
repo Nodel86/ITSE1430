c0dbea9e6af407a8e453983087e50388e1ef0bd9
Ashton Harris <ashton.harris@my.tccd.edu>
2018-05-04 20:31:22 -0500

    changes

--- Labs/Lab4/AshtonHarris.MovieLib.Windows/MainForm.cs
+++ Labs/Lab4/AshtonHarris.MovieLib.Windows/MainForm.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿//////////////
+////Ashton Harris
+////ITSE 1430
+////Lab4
+//////////////
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
